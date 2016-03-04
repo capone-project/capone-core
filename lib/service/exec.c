fe06c2cd01dd2411bbd1a4946160fcad1586ac75
Patrick Steinhardt <ps@pks.im>
2016-03-04 11:29:28 +0100

    sd: service: properly implement exec service

--- lib/service/exec.c
+++ lib/service/exec.c
@@ -22,6 +22,7 @@
 
 #include "lib/common.h"
 #include "lib/channel.h"
+#include "lib/log.h"
 #include "lib/service.h"
 
 static const char *version(void)
@@ -44,68 +45,111 @@ static int parameters(const struct sd_service_parameter **out)
 
 static int invoke(struct sd_channel *channel)
 {
-    int n;
-    uint8_t buf[4096];
-
-    while ((n = sd_channel_receive_data(channel, buf, sizeof(buf))) > 0) {
-        printf("%.*s", n, buf);
+    if (sd_channel_relay(channel, 1, STDOUT_FILENO) < 0) {
+        return -1;
     }
 
     return 0;
 }
 
+static void exec(const char *cmd,
+        const char **args, int nargs,
+        const char **envs, int nenvs)
+{
+    char **argv = NULL;
+    int i;
+
+    if (nargs > 0) {
+        argv = malloc(sizeof(char * const) * nargs + 2);
+
+        argv[0] = strdup(cmd);
+        for (i = 0; i < nargs; i++) {
+            argv[i + 1] = strdup(args[i]);
+        }
+        argv[nargs + 1] = NULL;
+    }
+
+    for (i = 0; i < nenvs; i++) {
+        char *name, *value;
+
+        if (strchr(envs[i], '=') == NULL)
+            continue;
+
+        name = strdup(envs[i]);
+        value = strchr(name, '=');
+        *value = '\0';
+        value++;
+
+        setenv(name, value, 0);
+
+        free(name);
+    }
+
+    execvp(cmd, argv);
+
+    exit(0);
+}
+
 static int handle(struct sd_channel *channel,
         const struct sd_service_parameter *params, size_t nparams)
 {
-    const char *cmd, **args, **env;
-    char **argv = NULL, **envp = NULL;
-    int pid, fds[2], nargs, nenv;
+    const char *cmd, **args = NULL, **envs = NULL;
+    int pid, nargs, nenvs;
+    int stdout_fds[2] = { -1, -1 }, stderr_fds[2] = { -1, -1 };
+    int error = 0;
 
     if (sd_service_parameters_get_value(&cmd, "command", params, nparams) < 0) {
-        puts("Missing 'command' parameter");
+        sd_log(LOG_LEVEL_ERROR, "Missing 'command' parameter");
         return -1;
     }
 
     nargs = sd_service_parameters_get_values(&args, "arg", params, nparams);
-    if (nargs > 0) {
-        argv = malloc(sizeof(char * const) * nargs + 1);
-        memcpy(argv, args, sizeof(char *) * nargs);
-        argv[nargs] = NULL;
-    }
+    nenvs = sd_service_parameters_get_values(&envs, "env", params, nparams);
 
-    nenv = sd_service_parameters_get_values(&env, "env", params, nparams);
-    if (nenv > 0) {
-        envp = malloc(sizeof(char * const) * nenv + 1);
-        memcpy(envp, args, sizeof(char *) * nenv);
-        envp[nenv] = NULL;
-    }
-
-    if (pipe(fds) < 0) {
-        puts("Unable to create pipe to child");
-        return -1;
+    if ((error = pipe(stdout_fds)) < 0 ||
+            (error = pipe(stderr_fds)) < 0)
+    {
+        sd_log(LOG_LEVEL_ERROR, "Unable to create pipes to child");
+        goto out;
     }
 
     pid = fork();
-    if (pid == 0) {
-        dup2(fds[1], STDOUT_FILENO);
-        close(fds[0]);
-        close(fds[1]);
-
-        execvpe(cmd, argv, envp);
-    } else if (pid > 0) {
-        uint8_t buf[4096];
-        int ret;
-
-        close(fds[1]);
-
-        while ((ret = read(fds[0], buf, sizeof(buf))) > 0) {
-            sd_channel_write_data(channel, buf, ret);
-        }
-    } else {
-        return -1;
+    if (pid < 0) {
+        sd_log(LOG_LEVEL_ERROR, "Unable to fork");
+        error = -1;
+        goto out;
     }
 
-    return 0;
+    if (pid == 0) {
+        dup2(stdout_fds[1], STDOUT_FILENO);
+        close(stdout_fds[0]);
+        close(stdout_fds[1]);
+        dup2(stderr_fds[1], STDERR_FILENO);
+        close(stderr_fds[0]);
+        close(stderr_fds[1]);
+
+        exec(cmd, args, nargs, envs, nenvs);
+    } else {
+        close(stdout_fds[1]);
+        close(stderr_fds[1]);
+
+        if (sd_channel_relay(channel, 2, stdout_fds[0], stderr_fds[0]) < 0) {
+            sd_log(LOG_LEVEL_ERROR, "Unable to relay exec output");
+            error = -1;
+            goto out;
+        }
+    }
+
+out:
+    free(args);
+    free(envs);
+
+    if (stdout_fds[0] >= 0) close(stdout_fds[0]);
+    if (stdout_fds[1] >= 0) close(stdout_fds[1]);
+    if (stderr_fds[0] >= 0) close(stderr_fds[0]);
+    if (stderr_fds[1] >= 0) close(stderr_fds[1]);
+
+    return error;
 }
 
 int sd_exec_init_service(struct sd_service *service)
