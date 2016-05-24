3cf068a2c3eefe1242348ecb889181a8f0d8b2ac
Patrick Steinhardt <ps@pks.im>
2016-05-24 15:13:46 +0200

    scripts: docs: add script to update gh-pages

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -4,3 +4,26 @@ set -x
 set -e
 
 doxygen -s scripts/Doxyfile
+
+if test $# -eq 1 && test $1 = "publish"
+then
+    (
+        PAGES="$(realpath ../gh-pages)"
+
+        rm -rf "${PAGES}"
+        git clone -b gh-pages . "${PAGES}"
+
+        rm -rf "${PAGES}"/doxygen
+        cp -r doxygen/html "${PAGES}"/doxygen
+
+        cd "${PAGES}"
+
+        if ! git diff --exit-code
+        then
+            return
+        fi
+
+        git add doxygen
+        git commit --author='Travis <doxygen@travis>' -m 'Doxygen update'
+    )
+fi
