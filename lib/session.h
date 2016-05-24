21379c12675cf91a0600279fcfd7dc1a28b15b85
Patrick Steinhardt <ps@pks.im>
2016-05-24 12:47:33 +0200

    sd: session: document interface

--- lib/session.h
+++ lib/session.h
@@ -16,8 +16,43 @@
  */
 
 /**
- * \defgroup sd-log Logging
+ * \defgroup sd-session Session
  * \ingroup sd-lib
+ *
+ * @brief Module handling session management
+ *
+ * Sessions are used to encapsulate parameters associated with a
+ * concrete invocation of a service. Clients may request a
+ * session that for a certain service that may be later invoked
+ * by the client or another party to start using the service.
+ *
+ * When requesting a session, the client has to choose parameters
+ * associated with the session, which are passed on to the
+ * service as soon as the session is started.
+ *
+ * Furthermore, the session requester has to specify an identity
+ * for the invoker. This will most often be the requester
+ * himself, but may be another identity when the session should
+ * be invoked by someone else.
+ *
+ * Sessions represent a kind of capability. That is they bundle
+ * together a session identifier used to distinguish sessions
+ * with an invoker. When a client wants to start a session, it is
+ * checked if any capability is present with the session
+ * identifier given by the client and his identity. If such a
+ * session exists, then the client may invoke it, otherwise he
+ * will be refused.
+ *
+ * Sessions may be revoked by the session issuer. When an
+ * identity requests a session for another identity but later
+ * decides to revoke the capability, he may do so. As such, the
+ * invoker is part of the session in order to keep track of who
+ * is able to revoke the session.
+ *
+ * The session parameters can only be chosen by the session
+ * issuer. This is by design so that the invoker is not able to
+ * escalate privileges specified by the session's parameters.
+ *
  * @{
  */
 
@@ -28,31 +63,99 @@
 
 #include "lib/keys.h"
 
+/** @brief A session wrapping identities and parameters */
 struct sd_session {
+    /** @brief Session identifier used to distinguish sessions */
     uint32_t sessionid;
 
+    /** @brief Identity allowed to start the session */
     struct sd_sign_key_public invoker;
+    /** @brief Identity allowed to revoke the session */
     struct sd_sign_key_public issuer;
 
+    /** @brief Parameters chosen for the session */
     struct sd_service_parameter *parameters;
+    /** @brief Number of parameters */
     size_t nparameters;
 };
 
+/** @brief Initialize sessions
+ *
+ * Initializes structs required for session management. This
+ * should be invoked only once when the main executable is
+ * started.
+ *
+ * @return <code>0</code> on success, <code>-1</code> otherwise
+ */
 int sd_sessions_init(void);
 
+/** @brief Add a new session
+ *
+ * Add a new session with the given identities and parameters.
+ * This will add a new session to the pool of already established
+ * sessions.
+ *
+ * This function may fail if a session with the same session
+ * identifier and invoker has already been specified.
+ *
+ * @param[in] sessionid Identifier of the new session.
+ * @param[in] issuer Identity of the session creator.
+ * @param[in] invoker Identity of the entity able to start the
+ *            session.
+ * @param[in] params Parameters for the session.
+ * @param[in] nparams Number of parameters.
+ * @return <code>0</code> on success, <code>-1</code> otherwise
+ */
 int sd_sessions_add(uint32_t sessionid,
         const struct sd_sign_key_public *issuer,
         const struct sd_sign_key_public *invoker,
         const struct sd_service_parameter *params,
         size_t nparams);
+
+/** @brief Remove a session
+ *
+ * Remove a session from the pool of already established
+ * sessions.
+ *
+ * @param[out] out Pointer to store removed session at. May be
+ *             <code>NULL</code>.
+ * @param[in] sessionid Session identifier to search for.
+ * @param[in] identity Session invoker to search for.
+ * @return <code>0</code> on success, <code>-1</code> otherwise
+ */
 int sd_sessions_remove(struct sd_session *out,
         uint32_t sessionid,
         const struct sd_sign_key_public *identity);
+
+/** @brief Find a session by identifier and invoker
+ *
+ * Finds a session which matches the given session identifier and
+ * the session's invoker.
+ *
+ * @param[out] out Pointer to store found session at.
+ * @param[in] sessionid Session identifier to search for. May be
+ *            <code>NULL</code>.
+ * @param[in] identity Session invoker to search for.
+ * @return session identifier on success, <code>-1</code>
+ *         otherwise
+ */
 ssize_t sd_sessions_find(struct sd_session *out,
         uint32_t sessionid,
         const struct sd_sign_key_public *identity);
+
+/** @brief Remove all established sessions
+ *
+ * @return <code>0</code> on success, <code>-1</code> otherwise
+ */
 int sd_sessions_clear(void);
 
+/** @brief Free a session
+ *
+ * Free's storage associated with the session. This primarily
+ * includes the session's parameters.
+ *
+ * @param[in] session Session to free
+ */
 void sd_session_free(struct sd_session *session);
 
 #endif
