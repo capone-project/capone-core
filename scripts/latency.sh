428b5eaeb245b6b455431b4a12a142a1c09ede6f
Patrick Steinhardt <ps@pks.im>
2016-05-26 18:46:00 +0200

    scripts: latency: do tests for block lenghts

--- scripts/latency.sh
+++ scripts/latency.sh
@@ -14,24 +14,29 @@ set -e
 sudo tc qdisc delete dev lo root netem || true
 sudo tc qdisc add dev lo root netem
 
-echo "latency,connect,await"
+echo "latency,pkglen,connect,await"
 
-for latency in 0 2 20 25 50 100
+for latency in 0 2 20
 do
     sudo tc qdisc change dev lo root netem delay ${latency}ms
 
-    (
-        cd source/sd/build
-        make 2>&1 1>/dev/null
-    )
+    for pkglen in 64 128 256 512 1024 1500 2048 4096
+    do
+        sed -e "s/#define PACKAGE_LENGTH .*$/#define PACKAGE_LENGTH $pkglen/" -i source/sd/lib/channel.c
 
-    RESULT=$(./source/sd/build/sd-latency | awk '!(NR%2) {printf "%s,%s", $NF, p} {p=$NF}')
-    CONNECT=$(echo "${RESULT}" | cut -d ',' -f 1)
-    CONNECT=$(echo "scale=3; ${CONNECT} / 1000 / 1000" | bc)
-    AWAIT=$(echo "${RESULT}" | cut -d ',' -f 2)
-    AWAIT=$(echo "scale=3; ${AWAIT} / 1000 / 1000" | bc)
+        (
+            cd source/sd/build
+            make 2>&1 1>/dev/null
+        )
 
-    echo "${latency},${CONNECT},${AWAIT}"
+        RESULT=$(./source/sd/build/sd-latency | awk '!(NR%2) {printf "%s,%s", $NF, p} {p=$NF}')
+        CONNECT=$(echo "${RESULT}" | cut -d ',' -f 1)
+        CONNECT=$(echo "scale=3; ${CONNECT} / 1000 / 1000" | bc)
+        AWAIT=$(echo "${RESULT}" | cut -d ',' -f 2)
+        AWAIT=$(echo "scale=3; ${AWAIT} / 1000 / 1000" | bc)
+
+        echo "${latency},${pkglen},${CONNECT},${AWAIT}"
+    done
 done
 
 sudo tc qdisc delete dev lo root netem
