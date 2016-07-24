f93d7bcd68f8d64a84a1a89b6a2e650fff283eaf
Patrick Steinhardt <ps@pks.im>
2016-07-24 15:21:36 +0200

    scripts: capabilities: adjust to changed cap model

--- scripts/capabilities.sh
+++ scripts/capabilities.sh
@@ -4,7 +4,7 @@ set -e
 
 cd source/sd/build
 
-CONTROLLER_KEY=f67b81a00b5be62a983295a083116e77ed7293be1813a379ce2fc0d52512abed
+CONTROLLER_KEY=d0c09c8cd02d960c285ee9d616d809d86024c27b915f8b1ee21a9b8c127f611c
 
 CLIENT_CFG=../config/client.conf
 CLIENT_KEY=$(cat ${CLIENT_CFG} | sed -n 's/^public_key=\(.*\)$/\1/p')
@@ -18,13 +18,13 @@ CAP_PORT=1238
 SERVICE_CFG=../config/server.conf
 SERVICE_KEY=$(cat ${SERVICE_CFG} | sed -n 's/^public_key=\(.*\)$/\1/p')
 SERVICE_TYPE=exec
-SERVICE_ADDR=192.168.0.100
+SERVICE_ADDR=192.168.178.38
 SERVICE_PORT=1237
 SERVICE_PARAMS="service-parameters=command=ls
                 service-parameters=arg=-l
                 service-parameters=arg=/"
 
-CAP_SESSION=$(./sd-connect request \
+CAP_SESSION="$(./sd-connect request \
     ${CLIENT_CFG} \
     ${CLIENT_KEY} \
     ${CAP_KEY} \
@@ -36,16 +36,21 @@ CAP_SESSION=$(./sd-connect request \
     service-identity=${SERVICE_KEY} \
     service-address=${SERVICE_ADDR} \
     service-port=${SERVICE_PORT} \
-    ${SERVICE_PARAMS} | awk '{print $2}')
+    ${SERVICE_PARAMS})"
+CAP_ID="$(echo "$CAP_SESSION" | awk 'NR == 1 { print $2 }')"
+CAP_SECRET="$(echo "$CAP_SESSION" | awk 'NR == 2 { print $2 }')"
 
-SERVICE_SESSION=$(./sd-connect connect \
+SERVICE_SESSION="$(./sd-connect connect \
     ${CLIENT_CFG} \
     ${CAP_KEY} \
     ${CAP_ADDR} \
     ${CAP_PORT} \
     capabilities \
-    ${CAP_SESSION} \
-    request | tail -n1 | awk '{print $2}')
+    ${CAP_ID} \
+    ${CAP_SECRET} \
+    request)"
+SERVICE_ID="$(echo "$SERVICE_SESSION" | awk 'NR == 3 { print $2 }')"
+SERVICE_SECRET="$(echo "$SERVICE_SESSION" | awk 'NR == 4 { print $2 }')"
 
 ./sd-connect connect \
     ${CLIENT_CFG} \
@@ -53,4 +58,5 @@ SERVICE_SESSION=$(./sd-connect connect \
     ${SERVICE_ADDR} \
     ${SERVICE_PORT} \
     ${SERVICE_TYPE} \
-    ${SERVICE_SESSION}
+    ${SERVICE_ID} \
+    ${SERVICE_SECRET}
