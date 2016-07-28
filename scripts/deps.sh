cdb4708932708d01929bfde4a149d89252ef8e53
Patrick Steinhardt <ps@pks.im>
2016-07-28 12:47:16 +0200

    scripts: deps: quietly extract archives

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -18,7 +18,7 @@ fi
 (
     cd /tmp
     wget https://github.com/google/protobuf/releases/download/v2.5.0/protobuf-2.5.0.tar.bz2
-    tar -xvf protobuf-2.5.0.tar.bz2
+    tar -xf protobuf-2.5.0.tar.bz2
     cd protobuf-2.5.0
     autoreconf -fi
     ./configure --host="${TARGET}" --prefix="${PREFIX}"
@@ -30,7 +30,7 @@ fi
 (
     cd /tmp
     wget https://github.com/protobuf-c/protobuf-c/releases/download/v1.0.2/protobuf-c-1.0.2.tar.gz
-    tar -xvf protobuf-c-1.0.2.tar.gz
+    tar -xf protobuf-c-1.0.2.tar.gz
     cd protobuf-c-1.0.2
     autoreconf -fi
     ./configure --host="${TARGET}" --prefix="${PREFIX}"
@@ -42,7 +42,7 @@ fi
 (
     cd /tmp
     wget https://cmocka.org/files/1.0/cmocka-1.0.1.tar.xz
-    tar -xvf cmocka-1.0.1.tar.xz
+    tar -xf cmocka-1.0.1.tar.xz
     cd cmocka-1.0.1
     mkdir build
     cd build
@@ -56,7 +56,7 @@ fi
 (
     cd /tmp
     wget https://github.com/jedisct1/libsodium/releases/download/1.0.8/libsodium-1.0.8.tar.gz
-    tar -xvf libsodium-1.0.8.tar.gz
+    tar -xf libsodium-1.0.8.tar.gz
     cd libsodium-1.0.8
     autoreconf -fi
     ./configure --host="${TARGET}" --prefix="${PREFIX}"
